2ce76318add0f8f1922d71f7b8c4d69ed530b30d
Yongran <maiyongran@gmail.com>
2022-08-17 15:11:17 +0100

    add properties of Gallery

--- art_gallery/src/main/java/art_gallery/Gallery.java
+++ art_gallery/src/main/java/art_gallery/Gallery.java
@@ -1,4 +1,16 @@
 package art_gallery;
 
+import java.util.ArrayList;
+
 public class Gallery {
+
+    private String name;
+    private double till;
+    private ArrayList<Artwork> artworks;
+
+    public Gallery(String name, double till){
+        this.name = name;
+        this.till = till;
+        this.artworks = new ArrayList<>();
+    }
 }
