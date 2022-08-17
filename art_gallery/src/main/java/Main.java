30df91415371ed2a64dd4420ec43aaa0886fcebb
Yongran <maiyongran@gmail.com>
2022-08-17 17:43:04 +0100

    check getPrice method

--- art_gallery/src/main/java/Main.java
+++ art_gallery/src/main/java/Main.java
@@ -1,2 +1,12 @@
+import art_gallery.Artist;
+import art_gallery.Artwork;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Artist artist = new Artist("Vincent van Gogh");
+
+        Artwork artwork = new Artwork("The Starry Night", artist, 2000, 150);
+        System.out.println(artwork.getPrice());
+    }
 }
