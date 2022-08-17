16c9b40c2472b00e53c90fcb891bf722efcfbc0c
Yongran <maiyongran@gmail.com>
2022-08-17 15:22:34 +0100

    add properties of Customer

--- art_gallery/src/main/java/art_gallery/Customer.java
+++ art_gallery/src/main/java/art_gallery/Customer.java
@@ -1,4 +1,12 @@
 package art_gallery;
 
 public class Customer {
+
+    private String name;
+    private double wallet;
+
+    public Customer(String name, double wallet){
+        this.name = name;
+        this.wallet = wallet;
+    }
 }
