ed7817374d22ebd31ac28971fa12eff4c4002349
Yongran <maiyongran@gmail.com>
2022-08-17 18:00:27 +0100

    add Customer artwork collection

--- art_gallery/src/main/java/art_gallery/Customer.java
+++ art_gallery/src/main/java/art_gallery/Customer.java
@@ -1,13 +1,17 @@
 package art_gallery;
 
+import java.util.ArrayList;
+
 public class Customer {
 
     private String name;
     private double wallet;
+    private ArrayList<Artwork> purchasedArtworks;
 
     public Customer(String name, double wallet){
         this.name = name;
         this.wallet = wallet;
+        this.purchasedArtworks = new ArrayList<>();
     }
 
     public double getWallet(){
@@ -16,5 +20,9 @@ public class Customer {
 
     public void subtractWallet(double payment){
         wallet -= payment;
+    }
+
+    public void addPurchasedArtwork(Artwork artwork){
+        purchasedArtworks.add(artwork);
     }
 }
