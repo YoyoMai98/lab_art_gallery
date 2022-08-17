538acdd6f9fcd4c48eb66513a9ea1632ed0bc12e
Yongran <maiyongran@gmail.com>
2022-08-17 15:20:04 +0100

    add properties of Artwork

--- art_gallery/src/main/java/art_gallery/Artwork.java
+++ art_gallery/src/main/java/art_gallery/Artwork.java
@@ -1,4 +1,17 @@
 package art_gallery;
 
 public class Artwork {
+
+    private String title;
+    private Artist artist;
+    private double price;
+    private double nft;
+
+    public Artwork(String title, Artist artist, double price, double nft){
+        this.title = title;
+        this.artist = artist;
+        this.price = price;
+        this.nft = nft;
+    }
+
 }
