fa52f962171c22cd4479a800c800c69058896ebd
Yongran <maiyongran@gmail.com>
2022-08-17 19:55:19 +0100

    add canGetWallet test for Customer

--- art_gallery/src/test/java/CustomerTest.java
+++ art_gallery/src/test/java/CustomerTest.java
@@ -1,6 +1,18 @@
 import art_gallery.Customer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class CustomerTest {
 
     private Customer customer;
+
+    @Test
+    public void canGetWallet(){
+        this.customer = new Customer("John", 130);
+        double actual = customer.getWallet();
+        double expected = 130;
+        assertThat(actual).isEqualTo(expected);
+    }
 }
