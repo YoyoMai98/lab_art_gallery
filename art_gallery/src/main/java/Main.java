3999ca9ddd9845886155d03f49ae8e0a4b9ac1bc
Yongran <maiyongran@gmail.com>
2022-08-18 13:11:07 +0100

    delete useless codes

--- art_gallery/src/main/java/Main.java
+++ art_gallery/src/main/java/Main.java
@@ -1,10 +1,3 @@
-import art_gallery.Artist;
-import art_gallery.Artwork;
-import art_gallery.Customer;
-import art_gallery.Gallery;
-
-import java.util.ArrayList;
-
 public class Main {
 
     public static void main(String[] args) {
