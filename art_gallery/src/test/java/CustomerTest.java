52384a55652dca686918ee5beea205a24cda84b3
Yongran <maiyongran@gmail.com>
2022-08-18 11:22:01 +0100

    test canBuyArtwork method

--- art_gallery/src/test/java/CustomerTest.java
+++ art_gallery/src/test/java/CustomerTest.java
@@ -13,16 +13,36 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class CustomerTest {
 
     private Customer customer;
+    private Artist vanGogh;
+    private Artwork starryNight;
+    private Artwork irises;
     
     @BeforeEach
     public void setUp(){
-        this.customer = new Customer("John", 130);
+        this.customer = new Customer("John", 1300);
+        this.vanGogh = new Artist("Vincent van Gogh");
+        this.starryNight = new Artwork("The Starry Night", vanGogh, 2000, 150);
+        this.irises = new Artwork("Irises", vanGogh, 1200, 230);
     }
 
     @Test
     public void canGetWallet(){
         double actual = customer.getWallet();
-        double expected = 130;
+        double expected = 1300;
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void canBuyArtwork__true(){
+        boolean actual = customer.canBuyArtwork(irises);
+        boolean expected = true;
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void canBuyArtwork__false(){
+        boolean actual = customer.canBuyArtwork(starryNight);
+        boolean expected = false;
         assertThat(actual).isEqualTo(expected);
     }
     
