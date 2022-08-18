743e83a72c2c84555777f09b8b6709822e209d9e
Yongran <maiyongran@gmail.com>
2022-08-18 11:34:52 +0100

    test canSellArtwork method

--- art_gallery/src/test/java/GalleryTest.java
+++ art_gallery/src/test/java/GalleryTest.java
@@ -1,5 +1,6 @@
 import art_gallery.Artist;
 import art_gallery.Artwork;
+import art_gallery.Customer;
 import art_gallery.Gallery;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,12 +14,14 @@ public class GalleryTest {
     private Gallery gallery;
     private Artwork artwork;
     private Artist artist;
+    private Customer customer;
 
     @BeforeEach
     public void setUp(){
         this.artist = new Artist("Vincent van Gogh");
         this.gallery = new Gallery("YK Gallery", 1300);
         this.artwork = new Artwork("The Starry Night", artist, 2000, 150);
+        this.customer = new Customer("Harry", 3000);
     }
 
     @Test
@@ -26,6 +29,13 @@ public class GalleryTest {
         double actual = gallery.getTill();
         double expected = 1300;
         assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test void canSellArtwork(){
+        gallery.addArtwork(artwork);
+        gallery.stockTake();
+        assertThat(gallery.getTill()).isEqualTo(3300);
+        assertThat(customer.getWallet()).isEqualTo(1000);
     }
 
     // ext_2
