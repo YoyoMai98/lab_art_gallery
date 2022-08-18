20fa55726ecd781f17c1f0a5c3ad69aa5368f1d3
Yongran <maiyongran@gmail.com>
2022-08-18 13:10:40 +0100

    delete codes in Main

--- art_gallery/src/main/java/Main.java
+++ art_gallery/src/main/java/Main.java
@@ -8,24 +8,5 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        Artist artist = new Artist("Vincent van Gogh");
-
-        Artwork artwork_1 = new Artwork("The Starry Night", artist, 2000, 150);
-        System.out.println(artwork_1.getPrice());
-        Artwork artwork_2 = new Artwork("Irises", artist, 1300, 230);
-
-        Customer customer = new Customer("John", 3000);
-        System.out.println("Wallet: " + customer.getWallet());
-
-        Gallery gallery = new Gallery("YK Gallery", 1300);
-        gallery.addArtwork(artwork_1);
-        gallery.addArtwork(artwork_2);
-
-        System.out.println("Stock: " + gallery.stockTake());
-
-        gallery.sellArtwork(customer,artwork_1);
-        System.out.println("Wallet after purchasing: " + customer.getWallet());
-
-        System.out.println("Stock: " + gallery.stockTake());
     }
 }
